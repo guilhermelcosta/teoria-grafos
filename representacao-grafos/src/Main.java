8c85726dec09f8bf20a1e145022b1c5db90f0645
Guilherme Lage da costa <guilhermeldcosta@gmail.com>
2023-09-06 19:29:49 -0300

    refactor: refatoracao geral do codigo

--- representacao-grafos/src/Main.java
+++ representacao-grafos/src/Main.java
@@ -1,14 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*Todos os codigo contidos neste programa foram desenvolvidos por Guilherme Costa*/
+/*Para ajustes pontuais e duvidas, em geral, foi utilizado o AI Assistant do IntelliJ*/
+/*Aluno: Guilherme Lage da Costa*/
+/*Matricula: 792939*/
+/*Data de entrega da atividade: 10/09/2023*/
+/*Repositorio: https://github.com/guilhermelcosta/teoria-grafos*/
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        String filepath = "forks/graph-test-100.txt";
+        String filepath = "graphs/graph-test-100.txt";
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
         String input = bufferedReader.readLine();
         int vertices = Integer.parseInt(input.trim().split(" ")[0]);
@@ -17,20 +24,8 @@ public class Main {
         int[] destiny = new int[edges + 1];
         int[] pointer = new int[vertices + 2];
 
-        int position = 1;
-        while ((input = bufferedReader.readLine()) != null) {
-            String[] arrInput = input.trim().split(" ");
-            origin[position] = Integer.parseInt(arrInput[0]);
-            destiny[position] = Integer.parseInt(arrInput[arrInput.length - 1]);
-            position++;
-        }
-
-        // Compacta o array origin
-        pointer[1] = 1;
-        for (int i = 2; i < pointer.length - 2; i++) {
-            pointer[i] = getOriginPosition(i, origin);
-        }
-        pointer[pointer.length - 2] = edges + 1;
+        fillOriginAndDestinyArrays(bufferedReader, origin, destiny);
+        createPointer(pointer, origin, edges);
 
         System.out.print("Informe o vertice: ");
         int vertex = scanner.nextInt();
@@ -44,19 +39,53 @@ public class Main {
         switch (op) {
             case "I" -> System.out.println("Grau de saida: " + getVertexOutputDegree(vertex, pointer));
             case "II" -> System.out.println("Grau de entrada: " + getVertexInputDegree(vertex, destiny));
-            case "III" ->
-                    System.out.println("Conjunto de sucessores: " + Arrays.toString(getVertexSuccessors(vertex, pointer, destiny)));
-            case "IV" ->
-                    System.out.println("Conjunto de predecessores: " + Arrays.toString(getVertexPredecessors(vertex, pointer, origin, destiny)));
+            case "III" -> System.out.println("Conjunto de sucessores: " + Arrays.toString(getVertexSuccessors(vertex, pointer, destiny)));
+            case "IV" -> System.out.println("Conjunto de predecessores: " + Arrays.toString(getVertexPredecessors(vertex, pointer, origin, destiny)));
             default -> throw new Exception("Opcao invalida!");
         }
     }
 
     /**
-     * Retorna a posicao inicial de um vertice no array origin
+     * Insere os vertices do arquivo indicado em 'filepath' nos arrays origin e destiny
      *
-     * @param i      posicao inicial de busca, comecando em 2, pois a posicao 0 e descartada e 1 pre definida como 1
-     * @param origin array de busca
+     * @param bufferedReader bufferedReader com o filepath indicado
+     * @param origin         array com os vertices de origem
+     * @param destiny        array com os vertices de destino
+     * @throws IOException excecao do bufferedReader caso nao tenha mais linhas para serem lidas
+     */
+    public static void fillOriginAndDestinyArrays(BufferedReader bufferedReader, int[] origin, int[] destiny) throws IOException {
+
+        String input;
+        int position = 1;
+
+        while ((input = bufferedReader.readLine()) != null) {
+            String[] arrInput = input.trim().split(" ");
+            origin[position] = Integer.parseInt(arrInput[0]);
+            destiny[position] = Integer.parseInt(arrInput[arrInput.length - 1]);
+            position++;
+        }
+    }
+
+    /**
+     * Cria o array 'pointer', que aponta para as posicoes inicias de cada vertice em 'origin'
+     *
+     * @param pointer array que aponta para as posicoes inicias de cada vertice em 'origin'
+     * @param origin  array com os vertices de origem
+     * @param edges   numero de arestas do grafo
+     */
+    public static void createPointer(int[] pointer, int[] origin, int edges) {
+        pointer[1] = 1;
+        for (int i = 2; i < pointer.length - 1; i++) {
+            pointer[i] = getOriginPosition(i, origin);
+        }
+        pointer[pointer.length - 1] = edges + 1;
+    }
+
+    /**
+     * Retorna a primeira posicao na qual um vertice e verificado no array origin
+     *
+     * @param i      posicao busca
+     * @param origin array com os vertices de origem
      * @return posicao inicial de um vertice no array origin
      */
     private static int getOriginPosition(int i, int[] origin) {
@@ -71,7 +100,7 @@ public class Main {
      * Obtem o grau de saida de um determinado vertice
      *
      * @param vertex  vertice buscado
-     * @param pointer array que aponta para a posicao inicial do vertice no array destiny
+     * @param pointer array que aponta para as posicoes inicias de cada vertice em 'origin'
      * @return grau de saida de um determinado vertice
      */
     public static int getVertexOutputDegree(int vertex, int[] pointer) {
@@ -82,25 +111,32 @@ public class Main {
      * Obtem o grau de entrada de um determinado vertice
      *
      * @param vertex  vertice buscado
-     * @param destiny array de destino de cada arco
+     * @param destiny array com os vertices de destino
      * @return grau de entrada de um determinado vertice
      */
     public static int getVertexInputDegree(int vertex, int[] destiny) {
 
-        int vertexInputDegree = 0, position = 0;
-//        Faz uma copia do array destiny, para nao modificar os valores originais
+//        Para nao modificar o array 'destiny' original durante o quicksort, optei por criar uma copia
+        int vertexInputDegree = 0;
         int[] sortedDestiny = Arrays.copyOf(destiny, destiny.length);
+
         quickSort(sortedDestiny, 1, sortedDestiny.length - 1);
 
-        while (sortedDestiny[position] <= vertex) {
-            if (sortedDestiny[position] == vertex)
+        for (int j : sortedDestiny)
+            if (j == vertex)
                 vertexInputDegree++;
-            position++;
-        }
 
         return vertexInputDegree;
     }
 
+    /**
+     * Retorna o conjunto de sucessores de um determinado vertice
+     *
+     * @param vertex  vertice buscado
+     * @param pointer array que aponta para as posicoes inicias de cada vertice em 'origin'
+     * @param destiny array com os vertices de destino
+     * @return conjunto de sucessores de um determinado vertice
+     */
     public static int[] getVertexSuccessors(int vertex, int[] pointer, int[] destiny) {
 
         int[] successors = new int[getVertexOutputDegree(vertex, pointer)];
@@ -112,6 +148,15 @@ public class Main {
         return successors;
     }
 
+    /**
+     * Retorna o conjunto de predecessores de um determinado vertice
+     *
+     * @param vertex  vertice buscado
+     * @param pointer array que aponta para as posicoes inicias de cada vertice em 'origin'
+     * @param origin  array com os vertices de origem
+     * @param destiny array com os vertices de destino
+     * @return conjunto de sucessores de um determinado vertice
+     */
     public static int[] getVertexPredecessors(int vertex, int[] pointer, int[] origin, int[] destiny) {
 
         int inputDegree = getVertexInputDegree(vertex, destiny);
@@ -119,11 +164,9 @@ public class Main {
         int predecessorsCount = 0;
 
         for (int i = 1; i < pointer.length - 1; i++) {
-
 //            Se ja tiver encontrado a quantidade de predecessores previsto, finaliza o loop
             if (predecessorsCount == inputDegree)
                 break;
-
 //            Essa verificacao so pode ser inserida, pois o grafo e simples
 //            Caso ele nao fosse simples, e tivesse ciclos, todos os vertices deveriam ser verificadas
             if (i == vertex)
@@ -135,7 +178,6 @@ public class Main {
             for (int j = nextPosition; j < lastPosition; j++) {
                 if (destiny[j] == vertex) {
                     predecessors[predecessorsCount++] = origin[j];
-//                    Este break pode ser adicionado pelo mesmo motivo informado acima
 //                    Uma vez que nao existem ciclos no grafo, um vertice nao pode ser predecessor mais de uma vez de outro
                     break;
                 }
@@ -147,11 +189,11 @@ public class Main {
     /**
      * Imprime os arrays pointer e destiny
      *
-     * @param pointer array apontador para destiny
-     * @param destiny array que indica os vertices de destino
-     * @param edges   quantidade de arestas
+     * @param pointer array que aponta para as posicoes inicias de cada vertice em 'origin'
+     * @param destiny array com os vertices de destino
+     * @param edges   numero de arestas do grafo
      */
-    public static void printFork(int[] pointer, int[] destiny, int edges) {
+    public static void printGraph(int[] pointer, int[] destiny, int edges) {
         for (int i = 1; i < pointer.length - 1; i++)
             System.out.printf("[%3d]", pointer[i]);
 
@@ -162,7 +204,7 @@ public class Main {
     }
 
     /**
-     * Ordenacao por quicksort
+     * Realiza a ordenacao de um array por Quicksort
      *
      * @param arr  array a ser ordenado
      * @param low  posicao inicial de ordenacao
@@ -177,7 +219,7 @@ public class Main {
     }
 
     /**
-     * Cria uma nova particao a ser ordenada pelo metodo quicksort
+     * Cria uma nova particao a ser ordenada pelo metodo
      *
      * @param arr  array a ser ordenado
      * @param low  posicao inicial de ordenacao
@@ -203,8 +245,8 @@ public class Main {
      * Troca duas posicoes e define o novo pivo
      *
      * @param arr array a ser ordenado
-     * @param i   posicao atual da particao
-     * @param j   posicao atual do no array
+     * @param i   posicao atual da verificada pelo quicksort
+     * @param j   posicao atual da particao
      */
     private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
